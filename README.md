e83895d07c77ca42cf9a18affa976492ff09fef3
jiangtj <116749895@qq.com>
2020-09-02 17:29:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# hexo-renderer-marked-mini
+
+Yet another [marked] renderer for hexo with zero configuration.
+
+## Installation
+
+``` bash
+$ npm install hexo-renderer-marked-mini
+```
+
+## Options
+
+You can configure this plugin in `_config.yml`.
+
+``` yaml
+marked:
+  # marked options
+  # see: https://marked.js.org/using_advanced#options
+```
+
+## Extras
+
+This plug-in provides the same extensibility as [hexo-renderer-marked](https://github.com/hexojs/hexo-renderer-marked) does, Look at [extensibility](https://github.com/hexojs/hexo-renderer-marked#extensibility) chapter.
---
+++ index.js
@@ -0,0 +1,17 @@
+/* global hexo */
+
+'use strict';
+
+const renderer = require('./lib/renderer');
+
+hexo.config.marked = Object.assign({
+  isHexoRendererMarkedMini: true
+}, hexo.config.marked);
+
+hexo.extend.renderer.register('md', 'html', renderer, true);
+hexo.extend.renderer.register('markdown', 'html', renderer, true);
+hexo.extend.renderer.register('mkd', 'html', renderer, true);
+hexo.extend.renderer.register('mkdn', 'html', renderer, true);
+hexo.extend.renderer.register('mdwn', 'html', renderer, true);
+hexo.extend.renderer.register('mdtxt', 'html', renderer, true);
+hexo.extend.renderer.register('mdtext', 'html', renderer, true);
